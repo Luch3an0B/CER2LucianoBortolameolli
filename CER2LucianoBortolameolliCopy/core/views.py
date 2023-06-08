3ab7ba0707e1e0fd90c3ecb67f6db057ff43cf43
Luch3an0B <127700128+Luch3an0B@users.noreply.github.com>
2023-06-08 16:50:25 -0400

    Add files via upload

---
+++ CER2LucianoBortolameolliCopy/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+#from django.contrib.auth.models import User
+
+
+# Create your views here.
+
+
+def PagHomeIndex(request):
+    return render(request, "core/home.html")
+
+
+
+#def RegistroIndex(request):
+    #if (request.POST):
+        
+       # nombre_usuario=request.POST[User.username]
+      #  correo_usuario=request.POST[User.]
+
+        #usuario_trabajador=User.objects.create_user[]
+   # if request.POST=='POST':
+    #    form=UserCreationForm()
+     #   if form.is_valid():
+      #      form.save()
